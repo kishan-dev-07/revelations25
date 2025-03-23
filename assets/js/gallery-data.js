fc802e0bb40c7b0a600bc455c5348136c984629c
TusharGhosh56 <tusharghosh408@gmail.com>
2025-03-23 11:47:59 +0530

    updated gallery with 2025 images

---
+++ assets/js/gallery-data.js
@@ -0,0 +1,111 @@
+const galleryData = {
+    "2024": [
+        {
+            "image": "./assets/imgs/gallery/inauga.JPG",
+            "title": "Opening Ceremony",
+            "description": "Revelations '24"
+        },
+        {
+            "image": "./assets/imgs/gallery/charades.JPG",
+            "title": "Charades",
+            "description": "Revelations '24"
+        },
+        {
+            "image": "./assets/imgs/gallery/coddindebug.JPG",
+            "title": "Coding/Debugging",
+            "description": "Revelations '24"
+        },
+        {
+            "image": "./assets/imgs/gallery/football.JPG",
+            "title": "Sports",
+            "description": "Revelations '24"
+        },
+        {
+            "image": "./assets/imgs/gallery/it1.JPG",
+            "title": "IT Manager",
+            "description": "Revelations '24"
+        },
+        {
+            "image": "./assets/imgs/gallery/itquiz.JPG",
+            "title": "IT Quiz",
+            "description": "Revelations '24"
+        },
+        {
+            "image": "./assets/imgs/gallery/popquiz.JPG",
+            "title": "Pop Quiz",
+            "description": "Revelations '24"
+        },
+        {
+            "image": "./assets/imgs/gallery/prompt.JPG",
+            "title": "Prompt Engineering",
+            "description": "Revelations '24"
+        },
+        {
+            "image": "./assets/imgs/gallery/uiux.JPG",
+            "title": "UI/UX",
+            "description": "Revelations '24"
+        },
+        {
+            "image": "./assets/imgs/gallery/weebwars.JPG",
+            "title": "Weeb Wars",
+            "description": "Revelations '24"
+        },
+        {
+            "image": "./assets/imgs/gallery/valedictory.JPG",
+            "title": "Award Ceremony",
+            "description": "Revelations '24"
+        },
+    ],
+    "2025": [
+        {
+            "image": "./assets/imgs/gallery/2025/theme.jpg",
+            "title": "Theme",
+            "description": "Revelations '25"
+        },
+        {
+            "image": "./assets/imgs/gallery/2025/inaug.JPG",
+            "title": "Inauguration",
+            "description": "Revelations '25"
+        },
+        {
+            "image": "./assets/imgs/gallery/2025/emcee.JPG",
+            "title": "Opening Ceremony",
+            "description": "Revelations '25"
+        },
+        {
+            "image": "./assets/imgs/gallery/2025/football.JPG",
+            "title": "Sports",
+            "description": "Revelations '25"
+        },
+        {
+            "image": "./assets/imgs/gallery/2025/IMG_7728.JPG",
+            "title": "Cosplay Dance",
+            "description": "Revelations '25"
+        },
+        {
+            "image": "./assets/imgs/gallery/2025/group3.JPG",
+            "title": "Dream Team",
+            "description": "Revelations '25"
+        },
+        {
+            "image": "./assets/imgs/gallery/2025/info.JPG",
+            "title": "Guest of Honour",
+            "description": "Revelations '25"
+        },
+        {
+            "image": "./assets/imgs/gallery/2025/kishan.JPG",
+            "title": "Website Developer",
+            "description": "Revelations '25"
+        },
+        {
+            "image": "./assets/imgs/gallery/2025/teachgroup.JPG",
+            "title": "Group Photo",
+            "description": "Revelations '25"
+        },
+        {
+            "image": "./assets/imgs/gallery/2025/website.JPG",
+            "title": "Website",
+            "description": "Revelations '25"
+        }
+    ]
+};
